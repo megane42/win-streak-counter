126d235bfda942298ec2b02ac398d1d468bb8133
megane42 <h.kzm.gm@gmail.com>
2021-05-04 22:50:39 +0900

    integrate Counter into App

--- src/app.js
+++ src/app.js
@@ -1,9 +1,25 @@
-import Counter from "./counter"
+import { useState, useEffect } from 'react'
+import { subscribe }           from "./realtime_storage"
+import AddButton               from "./add_button"
+import WinStreak               from "./win_streak"
+import History                 from "./history"
+import Stats                   from "./stats"
 
 export default function App() {
+  const [ matchResults, setMatchResults ] = useState([])
+
+  useEffect(() => { subscribe(setMatchResults) }, [])
+
   return (
     <div>
-      <Counter />
+      <Stats     matchResults={matchResults} />
+      <History   matchResults={matchResults} numOfHistory={5} />
+      <WinStreak matchResults={matchResults} />
+      <p>
+        <AddButton matchResult="win"  />
+        <AddButton matchResult="lose" />
+        <AddButton matchResult="draw" />
+      </p>
     </div>
   )
 }
