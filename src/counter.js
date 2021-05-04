388dea0e13461ed8ae87bccfe5a4fe7a9a874461
megane42 <h.kzm.gm@gmail.com>
2021-05-04 19:59:52 +0900

    add 'order by' to query

--- src/counter.js
+++ src/counter.js
@@ -5,7 +5,7 @@ export default function Counter() {
   const [ results, setResults ] = useState([])
 
   useEffect(() => {
-    firebase.firestore().collection("results").onSnapshot((querySnapshot) => {
+    firebase.firestore().collection("results").orderBy("created_at", "desc").onSnapshot((querySnapshot) => {
       setResults(querySnapshot.docs)
     })
   }, [])
