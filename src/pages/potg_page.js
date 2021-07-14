45b67d140d15945fc38c81bea835462a34082ed2
megane42 <h.kzm.gm@gmail.com>
2021-07-14 22:46:26 +0900

    enable to record potg

--- src/pages/potg_page.js
+++ src/pages/potg_page.js
@@ -1,18 +1,22 @@
 import { useState, useEffect } from 'react'
 import queryString             from 'query-string'
 
-import { subscribe }           from '../lib/match_results_storage'
-import AddMatchResultButton    from '../components/add_match_result_button'
-import ToggleBackgroundButton  from '../components/toggle_background_button'
-import Potg                    from '../components/potg'
-import History                 from '../components/history'
-import Stats                   from '../components/stats'
-import styles                  from './potg_page.module.css'
+import { subscribe as subscribeMatchResult } from '../lib/match_results_storage'
+import { subscribe as subscribePotgResult }  from '../lib/potg_results_storage'
+import AddMatchResultButton                  from '../components/add_match_result_button'
+import AddPotgResultButton                   from '../components/add_potg_result_button'
+import ToggleBackgroundButton                from '../components/toggle_background_button'
+import Potg                                  from '../components/potg'
+import History                               from '../components/history'
+import Stats                                 from '../components/stats'
+import styles                                from './potg_page.module.css'
 
 export default function PotgPage() {
   const [ matchResults, setMatchResults ] = useState([])
+  const [ potgResults, setPotgResults ] = useState([])
 
-  useEffect(() => { subscribe(setMatchResults) }, [])
+  useEffect(() => { subscribeMatchResult(setMatchResults) }, [])
+  useEffect(() => { subscribePotgResult(setPotgResults) }, [])
 
   const [ backgroundTransparent, setBackgroundTransparent ] = useState(true)
 
@@ -23,7 +27,7 @@ export default function PotgPage() {
       <div className={styles.main}>
         <div className={styles.row}>
           <div className={styles.winStreak}>
-            <Potg potgResults={matchResults} targetPotg={targetPotg}/>
+            <Potg potgResults={potgResults} targetPotg={targetPotg}/>
           </div>
         </div>
 
@@ -44,7 +48,7 @@ export default function PotgPage() {
           <AddMatchResultButton matchResult='lose' />
         </span>
         <span className={styles.addButton}>
-          <AddMatchResultButton matchResult='draw' />
+          <AddPotgResultButton />
         </span>
         <span className={styles.addButton}>
           <ToggleBackgroundButton onClick={ () => { setBackgroundTransparent(!backgroundTransparent) } } />
